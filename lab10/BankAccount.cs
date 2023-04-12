ec188ea0a25610fddce30275216d4fc6063518bc
Justin Nicholas Dubin <99613905+Jdubin1417@users.noreply.github.com>
2023-04-12 09:12:24 -0400

    Add files via upload

---
+++ lab10/BankAccount.cs
@@ -0,0 +1,77 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: BankAccount.cs
+* Project name: lab9
+*--------------------------------------------------------------------
+* Author’s name and email: Justin Dubin, dubinj@etsu.edu
+* Course-Section: CSCI 1250-001
+* Creation Date: 20 Apr 2022
+*
+-------------------------------------------------------------------
+*/
+public class BankAccount
+{
+    private int accountNumber;
+    private char type;
+    private decimal balance;
+    public string FirstName;
+    public string LastName;
+
+
+
+    public BankAccount(char type, decimal initialDeposit, int accountNumber, string firstName, string lastName)
+    {
+         _name = name;
+        _address = address;
+        _age = age;
+        _phoneNumber = phoneNumber;
+    }
+
+    public BankAccount(obj BankAccount)
+    {
+         _name = name;
+        _address = address;
+        _age = age;
+        _phoneNumber = phoneNumber;
+    }
+
+    public int GetAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+
+    public string GetAccountTypeAsAString()
+    {
+        return type;
+    }
+
+    public bool Deposit(decimal amount)
+    {
+        
+    }
+
+    public bool Withdrawal(decimal amount)
+    {
+        
+    }
+
+    public decimal MonthlyInterest()
+    {
+        
+    }
+
+    public bool Equals(obj object)
+    {
+        
+    }
+
+    public string ToString()
+    {
+        
+    }
+}
---
+++ lab10/Program.cs
@@ -0,0 +1,30 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: Program.cs
+* Project name: lab9
+*--------------------------------------------------------------------
+* Author’s name and email: Justin Dubin, dubinj@etsu.edu
+* Course-Section: CSCI 1250-001
+* Creation Date: 20 Apr 2022
+*
+-------------------------------------------------------------------
+*/
+public class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Enter an initial deposit: ");
+        var deposit = Console.ReadLine();
+        
+
+        System.Console.WriteLine();
+        System.Console.Write("Enter your First Name: ");
+        string FirstName = Console.ReadLine();
+
+        System.Console.WriteLine();
+        System.Console.Write("Enter your Last Name: ");
+        string LastName = Console.ReadLine();
+    }
+
+
+}
